49322df90e85072fe9e48d1664f90536d270036d
Guido2k <Krypto.taco@gmail.com>
2020-10-25 21:48:36 -0600

    Updates

---
+++ precache-manifest.bca0f454954fef9b902ed0dbd2a72c28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d15cd4e7b59a8a7b59a310743fec010",
+    "url": "/Guido2k/moviehunter/index.html"
+  },
+  {
+    "revision": "32a4a1cea818727d1e35",
+    "url": "/Guido2k/moviehunter/static/css/main.339f8f77.chunk.css"
+  },
+  {
+    "revision": "5234703d9acb2232744e",
+    "url": "/Guido2k/moviehunter/static/js/2.3bc27ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Guido2k/moviehunter/static/js/2.3bc27ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a4a1cea818727d1e35",
+    "url": "/Guido2k/moviehunter/static/js/main.5885ffb7.chunk.js"
+  },
+  {
+    "revision": "9f8cdedced69b477a61a",
+    "url": "/Guido2k/moviehunter/static/js/runtime-main.670d2f70.js"
+  },
+  {
+    "revision": "079b4f64afda16c518bb573350fa0ec8",
+    "url": "/Guido2k/moviehunter/static/media/moviebg2.079b4f64.png"
+  },
+  {
+    "revision": "08392f186e99a74794065effff6f90e5",
+    "url": "/Guido2k/moviehunter/static/media/moviedblogo.08392f18.svg"
+  }
+]);
